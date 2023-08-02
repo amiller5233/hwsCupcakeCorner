43daf9d902d361c9ef4e5b1c23fe851336528057
amiller5233@gmail.com <amiller5233@gmail.com>
2023-08-01 19:22:48 -0700

    create xcode project

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Adam Miller on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
